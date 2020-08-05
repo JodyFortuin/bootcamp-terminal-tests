c8b3c5efa0611af33f8504f44543fa4517082f27
JodyFortuin <60154410+JodyFortuin@users.noreply.github.com>
2020-08-05 15:10:42 +0200

    Add files via upload

---
+++ totalPhoneBill.js
@@ -0,0 +1,12 @@
+function totalPhoneBill(bill){
+  var callLog = bill.split(', ')
+     var phoneBill = 0;
+  for(var i=0; i < callLog.length; i++)
+   
+    if (callLog[i] === 'call'){
+       phoneBill += 2.75;
+    } else if (callLog[i] === 'sms'){
+    phoneBill += 0.65;
+  } var roundedPhoneBill= 'R' + phoneBill.toFixed(2);
+  return 'R' + phoneBill.toFixed(2);
+}
